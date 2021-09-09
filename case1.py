6fe75813a4ee24d5329851d4409960f989eedf4e
Владимир <ulladimil@gmail.com>
2021-09-09 12:51:48 +0300

    Update case1.py

--- case1.py
+++ case1.py
@@ -1,32 +1,39 @@
 from django.db import models
 
 
-class ModelA(models.Model):
+class User(models.Model):
 
     id = models.AutoField()
     name = models.TextField()
-    model_b = models.ForeignKey(ModelB)
+    city = models.ForeignKey(city)
 
     def __str__(self):
-        return self.name
+        return f'User: {self.name}'
 
 
-class ModelB(models.Model):
+class City(models.Model):
 
     id = models.AutoField()
+    name = models.CharField(max_length=20)
 
     def __str__(self):
-        return f'Model B: {self.id}'
+        return f'City: {self.name}'
 
 
-for i in ModelB.objects.filter(modela__name__icontains='super model'):
-    print(i.id)
+for user in User.objects.filter(city__name__icontains='ново'):
+    print(user.name)
 
 
 '''
 Дано:
-    1 запись ModelB
-    3 записи ModelA которые ссылаются на ModelB c name 'Super model A first', 'Super model A second' и 'Super model A third'
+    3 записи City c полями name 'Новороссийск', 'Новоотрадное' и 'Новоселово'
+    1 запись User c name 'Иванов' и city которое ссылается на запись 'Новороссийск'
 
-В консоль выводятся дубликаты идентификаторов ModelB, где ошибка?
+В консоль выводится: 
+    Иванов 
+    Иванов 
+    Иванов 
+
+Почему так если пользователь один? 
+Что сделать чтобы пользователь выводился один раз?
 '''
